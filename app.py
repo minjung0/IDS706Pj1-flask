0639db272fc2c57913d7091e15a3d063bc836189
Minjung Lee <minjung.lee@duke.edu>
2021-09-21 01:45:08 +0000

    update 9/20~9/24

--- app.py
+++ app.py
@@ -1,49 +1,3 @@
-'''
-from flask import Flask
-from flask import jsonify
-app = Flask(__name__)
-
-def change(amount):
-    # calculate the resultant change and store the result (res)
-    res = []
-    coins = [1,5,10,25] # value of pennies, nickels, dimes, quarters
-    coin_lookup = {25: "quarters", 10: "dimes", 5: "nickels", 1: "pennies"}
-
-    # divide the amount*100 (the amount in cents) by a coin value
-    # record the number of coins that evenly divide and the remainder
-    coin = coins.pop()
-    num, rem  = divmod(int(amount*100), coin)
-    # append the coin type and number of coins that had no remainder
-    res.append({num:coin_lookup[coin]})
-
-    # while there is still some remainder, continue adding coins to the result
-    while rem > 0:
-        coin = coins.pop()
-        num, rem = divmod(rem, coin)
-        if num:
-            if coin in coin_lookup:
-                res.append({num:coin_lookup[coin]})
-    return res
-
-
-@app.route('/')
-def hello():
-    """Return a friendly HTTP greeting."""
-    print("I am inside hello world")
-    return 'Hello World! I can make change at route: /change'
-
-@app.route('/change/<dollar>/<cents>')
-def changeroute(dollar, cents):
-    print(f"Make Change for {dollar}.{cents}")
-    amount = f"{dollar}.{cents}"
-    result = change(float(amount))
-    return jsonify(result)
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
-'''
-
 from flask import Flask
 from flask import jsonify
 app = Flask(__name__)
@@ -51,15 +5,20 @@ app = Flask(__name__)
 def outputAssignment(name):
     res = ""
     if name == "IDS702":
-        res = "Data Analysis Assignment 2 (~Sep 14)"
+#        res = "Data Analysis Assignment 2 (~Sep 14)"
+        res = "Data Analysis Assignment 3 (~9/22)"
     elif name == "IDS703":
-        res = "Markov Text Generation (~Sep 15)"
+#        res = "Markov Text Generation (~Sep 15)"
+        res = "Part-of-Speech Tagging (~9/22)"
     elif name == "IDS706":
-        res = "Module Four Discussion (~Sep 15), Demo Video Assignment (~Sep 17)"
+#        res = "Module Four Discussion (~Sep 15), Demo Video Assignment (~Sep 17)"
+        res = "Module Four Discussion (~9/22), Demo Video Assignment (~9/24)"
     elif name == "IDS720":
-        res = "Pandas Series (~Sep 14), Dataframe Exercises (~Sep 16)"
+#        res = "Pandas Series (~Sep 14), Dataframe Exercises (~Sep 16)"
+        res = "Missing Values Exercises (~9/21), Github Exercises 1 (~9/21)"
     elif name == "IDS898":
-        res = "Modifiy your resume using check lists. (~ASAP)"
+#        res = "Modifiy your resume using check lists. (~ASAP)"
+        res = "Modifiy your resume using check lists. (~9/23)"
     elif name == "IDS791":
         res = "There is no assignment for this week! :)"
     else:
