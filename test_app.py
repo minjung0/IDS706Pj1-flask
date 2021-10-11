230f1ec14f0c11ee9821d3fb60699231d47b2363
Minjung Lee <minjung.lee@duke.edu>
2021-10-11 18:42:01 +0000

    edit test file

--- test_app.py
+++ test_app.py
@@ -2,4 +2,4 @@ from app import changeClass
 
 def test_change():
 #    assert ["Oops, I think it's not a classic MIDS class."] == changeClass('GS726')
-    assert changeClass('GS726') == "Oops, I think it's not a classic MIDS class."
+    assert changeClass('IDS791') == "There is no assignment for this week! :)"
