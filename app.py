b37f5d3e14f5fc86ec7c09cdb3bcbb77b9c9c70d
Minjung Lee <minjung.lee@duke.edu>
2021-10-11 18:22:26 +0000

    update assignments

--- app.py
+++ app.py
@@ -6,19 +6,24 @@ def outputAssignment(name):
     res = ""
     if name == "IDS702":
 #        res = "Data Analysis Assignment 2 (~Sep 14)"
-        res = "Data Analysis Assignment 3 (~9/22)"
+#        res = "Data Analysis Assignment 3 (~9/22)"
+        res = "Team Project 2 (~10/24)"
     elif name == "IDS703":
 #        res = "Markov Text Generation (~Sep 15)"
-        res = "Part-of-Speech Tagging (~9/22)"
+#        res = "Part-of-Speech Tagging (~9/22)"
+        res = "Document classification/TF-IDF (~10/11)"
     elif name == "IDS706":
 #        res = "Module Four Discussion (~Sep 15), Demo Video Assignment (~Sep 17)"
-        res = "Module Four Discussion (~9/22), Demo Video Assignment (~9/24)"
+#        res = "Module Four Discussion (~9/22), Demo Video Assignment (~9/24)"
+        res = "Module Eight Discussion (~10/13), Demo Video Assignment (~10/15)"
     elif name == "IDS720":
 #        res = "Pandas Series (~Sep 14), Dataframe Exercises (~Sep 16)"
-        res = "Missing Values Exercises (~9/21), Github Exercises 1 (~9/21)"
+#        res = "Missing Values Exercises (~9/21), Github Exercises 1 (~9/21)"
+        res = "Big Data Exercises (~10/19)"
     elif name == "IDS898":
 #        res = "Modifiy your resume using check lists. (~ASAP)"
-        res = "Modifiy your resume using check lists. (~9/23)"
+#        res = "Modifiy your resume using check lists. (~9/23)"
+        res = "There is no assignment for this week! :)"
     elif name == "IDS791":
         res = "There is no assignment for this week! :)"
     else:
